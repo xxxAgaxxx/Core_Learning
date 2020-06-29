e4e2ebd05ad3f795e141c88fa98481e242a1da13
Aga <bromaaga@gmail>
2020-06-30 04:10:03 +0700

    python mutable string update

--- 4.5.py
+++ 4.5.py
@@ -1,59 +1,67 @@
-class MutableString:
+class MutableString(str):
     def __init__(self, text):
-        self.mut_string = list(text)
+        self.mut_string = text
 
     def __str__(self):
-        return ''.join(self.mut_string)
-
-    def __len__(self):
-        return len(self.mut_string)
+        return self.mut_string
 
     def __getitem__(self, item):
         if isinstance(item, slice):
             return MutableString(self.mut_string[item.start:item.stop:item.step])
         else:
-            return self.mut_string[item]
+            return MutableString(self.mut_string[item])
 
     def __setitem__(self, key, value):
         if isinstance(key, slice):
             # если срез с шагом не единица
             if key.step not in (1, None):
+                temp_list = list(self.mut_string)
                 # пытаемся заменить не пустой строкой
                 if value != '':
                     # если число символов в строке равно числу символов в срезе — заменяем
                     if len(self.mut_string[key.start:key.stop:key.step]) == len(value):
-                        self.mut_string[key.start:key.stop:key.step] = value
+                        temp_list[key.start:key.stop:key.step] = value
+                        self.mut_string = MutableString(''.join(temp_list))
                     else:
                         raise ValueError(f'Нельзя заменить срез длины {len(self.mut_string[key.start:key.stop:key.step])} '
                                          f'на строку длины {len(value)}')
             else:
                 # шаг единица, не пустая строка — заменяем срез исходной строки
                 if value != '':
+                    self.mut_string = list(self.mut_string)
                     self.mut_string[key.start:key.stop:key.step] = value
+                    self.mut_string = MutableString(''.join(self.mut_string))
                 # шаг единица, не пустая строка — удаляем срез исходной строки
                 else:
+                    self.mut_string = list(self.mut_string)
                     del self.mut_string[key.start:key.stop:key.step]
+                    self.mut_string = MutableString(''.join(self.mut_string))
         elif value == '':
+            self.mut_string = list(self.mut_string)
             del self.mut_string[key]
+            self.mut_string = MutableString(''.join(self.mut_string))
         elif len(value) == 1:
             self.mut_string[key] = value
         else:
-            self.mut_string = self.mut_string[:key] + list(value) + self.mut_string[key + 1:]
+            self.mut_string = self.mut_string[:key] + value + self.mut_string[key + 1:]
 
     def __delitem__(self, key):
         if isinstance(key, slice):
+            self.mut_string = list(self.mut_string)
             del self.mut_string[key.start:key.stop:key.step]
+            self.mut_string = MutableString(''.join(self.mut_string))
         else:
+            self.mut_string = list(self.mut_string)
             del self.mut_string[key]
+            self.mut_string = MutableString(''.join(self.mut_string))
 
     def __add__(self, other):
-        return MutableString(self.mut_string + list(other))
+        return MutableString(self.mut_string + other)
 
     def __radd__(self, other):
         return MutableString(list(other) + self.mut_string)
 
-    def __mul__(self, other):
-        return MutableString(self.mut_string + self.mut_string)
+
 
 
 string = MutableString('mutable')  # инициализация mut_string = ['m', 'u', 't', 'a', 'b', 'l', 'e']
@@ -70,7 +78,7 @@ string = string[::-1]  # получение символов срезом mut_st
 print(string[:3])  # получение символов срезом lol
 del string[::2]  # удаление символов срезом mut_string = ['o', 'e', 'b', 't', 'm']
 print(string)  # oebtm
-print('kek' + string)  # конкатенация с другой стороны kekoebtm
 print(string * 2)  # питоновское умножение строк oebtmoebtm
 string[::2] = '123'  # замена среза на строку mut_string = ['1', 'e', '2', 't', '3']
 print(string)  # 1e2t3
+print(string.capitalize())
